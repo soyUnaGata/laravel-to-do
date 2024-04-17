c59eb3620e115dc8bdcb066622ac961d7eea0f22
soyUnaGata <fallen.snitch@gmail.com>
2024-04-17 21:16:20 +0300

    implement yield

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>
+            Laravel 10 Tasks List App
+        </title>
+    </head>
+
+    <body>
+        <h1>
+            @yield('title')
+        </h1>
+
+    <div>@yield('content')</div>
+    </body>
+</html>
