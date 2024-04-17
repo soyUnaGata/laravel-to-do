b78963fbd47cf0add0504f2fe0a8f08dec3ea6e2
soyUnaGata <fallen.snitch@gmail.com>
2024-04-17 20:55:12 +0300

    display task info

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,1 +1,9 @@
 <h1>{{$task->title}}</h1>
+<h1>{{$task->description}}</h1>
+
+@if($task->long_description)
+    <p>{{$task->long_description}}</p>
+@endif
+
+<p>{{$task->created_at}}</p>
+<p>{{$task->updated_at}}</p>
